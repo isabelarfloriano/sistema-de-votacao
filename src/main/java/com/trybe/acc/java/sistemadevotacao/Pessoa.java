ce061efc5f3372f6e00ff3f7df3f27a17275a82e
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-22 15:37:18 -0300

    feat: cria classe abstrata pessoa

--- src/main/java/com/trybe/acc/java/sistemadevotacao/Pessoa.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Pessoa.java
@@ -1,5 +1,18 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class Pessoa {
+/**
+ * classe abstrata.
+ *
+ */
+public abstract class Pessoa {
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
 }

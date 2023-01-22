e6c370ee5efcb0988421b2519c9ae283f253e719
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-22 18:16:02 -0300

    fix: lint

--- src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
@@ -13,31 +13,42 @@ public class GerenciamentoVotacao {
   private static ArrayList<String> cpfComputado = new ArrayList<String>();
   private int totalVotos;
 
+  /**
+   * cadastrarPessoaCandidata.
+   */
   public void cadastrarPessoaCandidata(String nome, int numero) {
     for (int i = 0; i < pessoasCandidatas.size(); i++) {
       if (pessoasCandidatas.get(i).getNumero() == numero) {
         System.out.println("Número pessoa candidata já utilizado!");
       } else {
-        pessoasCandidatas.add(new PessoaCandidata(nome,numero));
-      }      
+        pessoasCandidatas.add(new PessoaCandidata(nome, numero));
+      }
+    }
   }
 
+  /**
+   * cadastrarPessoaEleitora.
+   */
   public void cadastrarPessoaEleitora(String nome, String cpf) {
-      for (int i = 0; i < pessoasEleitoras.size(); i++) {
-        if (pessoasEleitoras.get(i).getCpf() == cpf) {
-          System.out.println("Pessoa eleitora já cadastrada!");
-        } else {
-          pessoasEleitoras.add(new PessoaEleitora(nome,cpf));
-        }      
+    for (int i = 0; i < pessoasEleitoras.size(); i++) {
+      if (pessoasEleitoras.get(i).getCpf() == cpf) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+      } else {
+        pessoasEleitoras.add(new PessoaEleitora(nome, cpf));
+      }
+    }
   }
 
+  /**
+   * votar.
+   */
   public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
     if (cpfComputado.contains(cpfPessoaEleitora)) {
       System.out.println("Pessoa eleitora já votou!");
     } else {
       for (int i = 0; i < pessoasCandidatas.size(); i++) {
-        if (pessoasCandidatas.get(i).getCpf() == numeroPessoaCandidata) {
-          pessoasCandidatas.get(i).receberVoto();
+        if (pessoasCandidatas.get(i).getNumero() == numeroPessoaCandidata) {
+          pessoasCandidatas.get(i).receberVotos();
           cpfComputado.add(cpfPessoaEleitora);
           totalVotos += 1;
         }
@@ -45,23 +56,25 @@ public class GerenciamentoVotacao {
     }
   }
 
+  /**
+   * metodo para ,pstrar resultado parcial e fianl da votação.
+   */
   public void mostrarResultado() {
-     if (totalVotos == 0) {
-        System.out.println("É preciso ter pelo menos um voto para mostrar o resultado.")
-     } else {     
-       for (int i = 0; i < pessoasCandidatas.size(); i++) {
-         if(pessoasCandidatas.get(i).getVotos() > 0) {
-           System.out.println("Nome: " + pessoasCandidatas.get(i).getNome() + " - ");
-           System.out.println(pessoasCandidatas.get(i).getVotos() + " votos ( ");
-           System.out.println(calcularPorcentagemVotos(i) + " )\n");
-         }
-       }
-       System.out.println("Total de votos: " + totalVotos);
-     }
+    if (totalVotos == 0) {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado.");
+    } else {
+      for (int i = 0; i < pessoasCandidatas.size(); i++) {
+        if (pessoasCandidatas.get(i).getVotos() > 0) {
+          System.out.println("Nome: " + pessoasCandidatas.get(i).getNome() + " - ");
+          System.out.println(pessoasCandidatas.get(i).getVotos() + " votos ( ");
+          System.out.println(calcularPorcentagemVotos(i) + " )\n");
+        }
+      }
+      System.out.println("Total de votos: " + totalVotos);
+    }
   }
-  
+
   private double calcularPorcentagemVotos(int indiceDoCandidato) {
-    return Math.round((pessoasCandidatas.get(indiceDoCandidato).getVotos()
-        / totalVotos) * 100);
+    return Math.round((pessoasCandidatas.get(indiceDoCandidato).getVotos() / totalVotos) * 100);
   }
 }

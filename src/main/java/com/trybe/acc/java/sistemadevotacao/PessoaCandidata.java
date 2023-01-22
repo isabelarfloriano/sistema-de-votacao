041445709f4b4c5b7ff89414b26dd3f9a08a562d
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-22 15:45:45 -0300

    feat: cria sub classe pessoa candidata

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,40 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaCandidata {
+/**
+ * classe herda da classe Pessoa.
+ *
+ */
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
+
+  /**
+   * metodo construtor.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    this.numero = numero;
+    super.nome = nome;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVotos() {
+    this.votos = this.votos + 1;
+  }
 
 }
